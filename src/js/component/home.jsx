a9171466ce1fd4c8a8bec78652346f9721cd98a8
Luis Lugo <124685188+JerryJox@users.noreply.github.com>
2023-04-20 10:59:59 +0000

    upload files

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./navbar.jsx"
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
 import Card from "./card.jsx";
